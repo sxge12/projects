04d2532a853d99ad80b7807202f757207f2e47f1
Crawford <crawfordclarke123@gmail.com>
2025-09-03 22:04:06 +0100

    Add files via upload

---
+++ personaldevelopment.py/pythonlearning.main.py
@@ -0,0 +1,35 @@
+#f = open( "usage.txt" )
+#otal = 0
+#for n in range( 3 ):
+  #n1 = int( f.readline() )
+  #print (n1)
+  #total = total + n1
+  #print(total)
+#print(otal)
+
+r = open("numbers.txt")
+
+for n in range(10):
+    total = 0
+    TTotal = 0
+    TimeRan = int(r.readline())
+    TimeWalked = int(r.readline())
+    totaltimeran = TimeRan + total + TimeRan
+    totaltimewalked = TimeWalked + TTotal + TimeWalked
+
+print(totaltimeran)
+print(totaltimewalked)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
